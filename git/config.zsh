6e48dbcc8dd1023ce678e3ddd2a91a97308c7236
samuelleach <samuel.leach@gmail.com>
2016-04-06 20:43:43 +0100

    Adding nbstripout filter

--- git/config.zsh
+++ git/config.zsh
@@ -5,3 +5,10 @@ git config --global alias.rollbackto "reset --hard"
 # For CSV diffs
 git config --global alias.csvdiff "diff --word-diff"
 git config --global diff.csv.wordRegex $'[^,\n]+[,\n]|[,]'
+
+# For Jupyter nbstripout (https://github.com/kynan/nbstripout)
+# Filtering requires a .gitattributes file with
+# *.ipynb filter=nbstripout
+git config filter.nbstripout.clean nbstripout
+git config filter.nbstripout.smudge cat
+git config filter.nbstripout.required true
