a26808dbb8676194d81c66116240a48bdf9fb3e9
samuelleach <samuel.leach@gmail.com>
2016-05-03 17:35:14 +0100

    Update

--- torch/config.zsh
+++ torch/config.zsh
@@ -1,1 +1,7 @@
+# Torch installation instructions from: http://torch.ch/docs/getting-started.html
+
+# git clone https://github.com/torch/distro.git ~/torch --recursive
+# cd ~/torch; bash install-deps;
+# ./install.sh
+
 . $HOME/torch/install/bin/torch-activate
