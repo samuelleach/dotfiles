e2352b8acf274f06e056957e2ac47c633fe4218d
samuelleach <samuel.leach@gmail.com>
2016-06-21 21:02:36 +0100

    Adding mkvirtualenv config

---
+++ python/config.zsh
@@ -0,0 +1,5 @@
+# virtualenvwapper (http://virtualenvwrapper.readthedocs.io/en/latest/install.html)
+
+export WORKON_HOME=~HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Devel
+source ~/anaconda3/bin/virtualenvwrapper.sh
