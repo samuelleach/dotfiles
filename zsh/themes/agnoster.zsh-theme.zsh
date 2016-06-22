e001ca4ce97cc58fea5048c36331261225937e3d
samuelleach <samuel.leach@gmail.com>
2016-06-22 21:33:53 +0100

    reformating time

--- zsh/themes/agnoster.zsh-theme.zsh
+++ zsh/themes/agnoster.zsh-theme.zsh
@@ -72,8 +72,9 @@ prompt_end() {
 ### Prompt components
 # Each component will draw itself, and hide itself if no information needs to be shown
 
+DATE=`date +"%T"`
 prompt_context() {
-    prompt_segment $PRIMARY_FG default "$HAPPYPERSON  "%T" "
+    prompt_segment $PRIMARY_FG default "$HAPPYPERSON  $DATE "
 }
 
 prompt_virtualenv() {
